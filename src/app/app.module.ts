69c27957c9915e26e2aa3ea4f103efd62bf7eed5
Patrick O'Mara <patrick.omara@cellsignal.com>
2018-04-03 09:50:39 -0400

    test API endpoints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -19,6 +19,7 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    HttpClientJsonpModule,
     FormsModule
   ],
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Injectable,Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { HttpRequest } from 'selenium-webdriver/http';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+@Injectable()
 export class HomeComponent implements OnInit {
   @ViewChild('table') table: ElementRef;
   @ViewChild('textRow') textRow: ElementRef;
@@ -23,8 +28,17 @@ export class HomeComponent implements OnInit {
 
   getSummary() {
 
-    const proxyurl = "https://test-project-again.herokuapp.com/";
     const url = "https://en.wikipedia.org/w/api.php?format=json&formatversion=2&action=query&prop=extracts&exintro=&explaintext=&titles=Star%20Trek"; // site that doesn’t send Access-Control-*
+
+    // Pass the key for your callback (in this case 'callback')
+    // as the second argument to the jsonp method
+    console.log("getSummary");
+    
+    this.http.jsonp(url, 'getSummaryCallback');
+    
+    
+    /*
+    const proxyurl = "https://test-project-again.herokuapp.com/";
     this.http.get<WikiPage>(url).subscribe 
     (data => {
       console.log(data.query.pages[0].extract);
@@ -35,7 +49,18 @@ export class HomeComponent implements OnInit {
         letterElement.innerHTML = text[i];
         this.textRow.nativeElement.appendChild(letterElement);;
       }
-    });
+    });*/
+  }
+
+  getSummaryCallback(jsonp) {
+    console.log(jsonp.query.pages[0].extract);
+      var text = jsonp.query.pages[0].extract;
+
+      for(var i = 0; i < text.length; i++) {
+        var letterElement = document.createElement("td");
+        letterElement.innerHTML = text[i];
+        this.textRow.nativeElement.appendChild(letterElement);;
+      }
   }
 }
 
